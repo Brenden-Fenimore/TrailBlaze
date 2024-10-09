dbd255669a64e569e05895d5d33e540868c8f75b
Mamie Gartin <megartin@student.fullsail.edu>
2024-10-09 03:51:00 -0400

    MainActivity.kt since we are no longer using the action bar I deleted the setupActionBarWithNavController
    i added strings to the string class
    
    added some more drawables
    
    created a SettingsScreenActivity.kt and started coding the buttons
    
    created the activity_settings_screen.xml and did the layout for the settings screen

--- app/src/main/java/com/example/trailblaze/MainActivity.kt
+++ app/src/main/java/com/example/trailblaze/MainActivity.kt
@@ -5,8 +5,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.trailblaze.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -32,7 +30,5 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_map, R.id.navigation_favorites, R.id.navigation_profile
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
