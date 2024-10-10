bf1c0631a4f0091307eb1f28c23a526b990f3ab9
Mamie Gartin <megartin@student.fullsail.edu>
2024-10-10 16:45:21 -0400

    MainActivity
    I guess i accidently deleted the navController from Main bc the app would not switch between fragments. So i had to set it back up

--- app/src/main/java/com/example/trailblaze/MainActivity.kt
+++ app/src/main/java/com/example/trailblaze/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.trailblaze
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 import com.example.trailblaze.login.LoginActivity
 import com.example.trailblaze.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,8 +35,12 @@ class MainActivity : AppCompatActivity() {
 
            // Passing each menu ID as a set of Ids because each
             // menu should be considered as top level destinations.
+            val navView: BottomNavigationView = binding.navView
 
-
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            navView.setupWithNavController(navController)
         }
     }
 }
