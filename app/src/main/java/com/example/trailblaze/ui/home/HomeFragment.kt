bc3aeb04e606214a06e8d9c9d1771ac2fe0ce180
lipeduh <fdesouza@student.fullsail.edu>
2024-10-12 17:02:41 -0400

    Got the app to build with Mamie's help

--- app/src/main/java/com/example/trailblaze/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/trailblaze/ui/home/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.trailblaze.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.trailblaze.databinding.FragmentHomeBinding
+import com.example.trailblaze.ui.MenuActivity
 
 class HomeFragment : Fragment() {
 
