cd962715e4c6e48f5e1c5a4444bc8db49a1284c8
lipeduh <fdesouza@student.fullsail.edu>
2024-10-13 13:43:15 -0400

    Handle settings button press on MenuActivity.kt to navigate to SettingsScreenActivity.kt

--- app/src/main/java/com/example/trailblaze/ui/MenuActivity.kt
+++ app/src/main/java/com/example/trailblaze/ui/MenuActivity.kt
@@ -1,9 +1,11 @@
 package com.example.trailblaze.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.trailblaze.R
 import android.widget.ImageButton
+import com.example.trailblaze.settings.SettingsScreenActivity
 
 // Activity for the menu screen
 class MenuActivity : AppCompatActivity() {
@@ -15,6 +17,11 @@ class MenuActivity : AppCompatActivity() {
         findViewById<ImageButton>(R.id.chevron_left).setOnClickListener{        // Find the ImageButton by its ID and set a click listener
             onBackPressedDispatcher.onBackPressed()     // When the button is clicked, go back to the previous activity
         }
-    }
 
+        // Handle settings button press and navigate to SettingsScreenActivity
+        findViewById<ImageButton>(R.id.imageButton).setOnClickListener {
+            val intent = Intent(this, SettingsScreenActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
