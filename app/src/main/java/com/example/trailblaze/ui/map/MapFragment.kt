bb99cd29729dd0029aeba982b65662e0ee1e6ed5
Brenden Fenimore <BGFenimore@student.fullsail.edu>
2024-10-15 17:12:01 -0700

    Added Searchbar to Map fragment using Google Places

--- app/src/main/java/com/example/trailblaze/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/trailblaze/ui/map/MapFragment.kt
@@ -1,6 +1,6 @@
 package com.example.trailblaze.ui.Map
 
-import android.content.Intent
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,13 +10,17 @@ import android.view.ViewGroup
 import com.example.trailblaze.BuildConfig.PLACES_API_KEY
 import com.example.trailblaze.R
 import com.example.trailblaze.databinding.FragmentMapBinding
-import com.example.trailblaze.settings.SettingsScreenActivity
+import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 class MapFragment : Fragment() {
 
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
+
     //Define a variable to hold the Places API key.
     val apiKey = PLACES_API_KEY
 
@@ -34,6 +38,8 @@ class MapFragment : Fragment() {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val fragmentManager = childFragmentManager
+
         // Log an error if apiKey is not set.
         if (apiKey.isEmpty() || apiKey == "DEFAULT_API_KEY") {
             Log.e("Places test", "No api key")
@@ -45,6 +51,27 @@ class MapFragment : Fragment() {
 
         // Create a new PlacesClient instance
         val placesClient = Places.createClient(context)
+
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment =
+            fragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as AutocompleteSupportFragment
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                //Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(status: Status) {
+                //Handle the error.
+                Log.i(TAG, "An error occurred: $status")
+            }
+        })
 
         return root
     }
