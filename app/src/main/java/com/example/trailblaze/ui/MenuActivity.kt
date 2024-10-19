4075eb715207160077d8e037bc6d0d755edcdecf
Brenden Fenimore <BGFenimore@student.fullsail.edu>
2024-10-18 17:29:13 -0700

    Halfway implemented the autocomplete functionality, searchbar works and clears but the call to the autocomplete server returns as a fail. Still have to edit that to get an updated dropdown menu and successful place calls

--- app/src/main/java/com/example/trailblaze/ui/MenuActivity.kt
+++ app/src/main/java/com/example/trailblaze/ui/MenuActivity.kt
@@ -14,5 +14,4 @@ class MenuActivity : AppCompatActivity() {
             onBackPressedDispatcher.onBackPressed()
         }
     }
-
 }
--- app/src/main/java/com/example/trailblaze/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/trailblaze/ui/map/MapFragment.kt
@@ -1,22 +1,46 @@
 package com.example.trailblaze.ui.Map
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.style.CharacterStyle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
+import androidx.annotation.Nullable
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.Fragment
 import com.example.trailblaze.BuildConfig.PLACES_API_KEY
 import com.example.trailblaze.R
 import com.example.trailblaze.databinding.FragmentMapBinding
-import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.*
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken
+import com.google.android.libraries.places.api.model.PlaceTypes.*
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsResponse
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.api.net.kotlin.awaitFindAutocompletePredictions
+import kotlinx.coroutines.tasks.await
+import java.util.*
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(),
+    OnCameraMoveStartedListener,
+OnCameraMoveListener,
+OnCameraMoveCanceledListener,
+OnCameraIdleListener,
+OnMapReadyCallback {
 
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
@@ -24,54 +48,55 @@ class MapFragment : Fragment() {
     //Define a variable to hold the Places API key.
     val apiKey = PLACES_API_KEY
 
+    private lateinit var map: GoogleMap
+    private var currPolylineOptions: PolylineOptions? = null
+    private var isCanceled = false
+    private val FullSail : CameraPosition = CameraPosition.builder().target(LatLng(28.596472,-81.301472)).zoom(15f).build()
+    var autocompletelist : List<AutocompletePrediction> = mutableListOf()
+    var autocompletelistString : MutableList<String> = mutableListOf("yo","bro","go","beep")
+    val autocompletePrimaryTypes = listOf("hiking_area", "park")
 
 
 
 
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val fragmentManager = childFragmentManager
-
-        // Log an error if apiKey is not set.
-        if (apiKey.isEmpty() || apiKey == "DEFAULT_API_KEY") {
-            Log.e("Places test", "No api key")
-            return root
-         }
-
-         //Initialize the SDK
-        Places.initialize(context, apiKey)
+        //Set the searchbar and clearbutton to their respective views
+        var multiAutoCompleteTextView = _binding!!.mapSearch.findViewById<MultiAutoCompleteTextView>(R.id.map_search)
+        var clearbutton = _binding!!.clearMapsearchtext.findViewById<ImageButton>(R.id.clear_mapsearchtext)
+        //Initialize the SDK
+        Places.initializeWithNewPlacesApiEnabled(context, apiKey)
 
         // Create a new PlacesClient instance
         val placesClient = Places.createClient(context)
 
-        // Initialize the AutocompleteSupportFragment.
-        val autocompleteFragment =
-            fragmentManager.findFragmentById(R.id.autocomplete_fragment)
-                    as AutocompleteSupportFragment
 
-        // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+        //clear searchbar when clearbutton is clicked
+        clearbutton?.setOnClickListener { multiAutoCompleteTextView?.text?.clear() }
 
-        // Set up a PlaceSelectionListener to handle the response.
-        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
-            override fun onPlaceSelected(place: Place) {
-                //Get info about the selected place.
-                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+        var autoFillAdapter =
+            context?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, autocompletelistString) }
+        multiAutoCompleteTextView.setAdapter(autoFillAdapter)
+        multiAutoCompleteTextView.setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
+        multiAutoCompleteTextView.threshold = 0
+
+
+        multiAutoCompleteTextView?.addTextChangedListener{
+
+            if(multiAutoCompleteTextView.text?.isNotEmpty()==true) {
+                UpdateMapDropDownMenu(multiAutoCompleteTextView.text.toString(), placesClient)
             }
+        }
 
-            override fun onError(status: Status) {
-                //Handle the error.
-                Log.i(TAG, "An error occurred: $status")
-            }
-        })
+        // Log an error if apiKey is not set.
+        if (apiKey.isEmpty()) {
+            Log.e("Places test", "No api key")
+            return root
+        }
 
         return root
     }
@@ -79,5 +104,51 @@ class MapFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+
+    fun UpdateMapDropDownMenu(string : String, placesClient: PlacesClient){
+
+        UpdateAutoCompletePredictions(string, placesClient)
+
+
+    }
+
+    //function to update list of autocomplete suggestions
+    fun UpdateAutoCompletePredictions(string : String, placesClient : PlacesClient){
+        val token = AutocompleteSessionToken.newInstance()
+        val findAutocompletePredictionsRequest  =
+            FindAutocompletePredictionsRequest.builder()
+                .setQuery(string)
+                .setTypesFilter(autocompletePrimaryTypes)
+                .setSessionToken(token)
+                .build()
+        placesClient.findAutocompletePredictions(findAutocompletePredictionsRequest).addOnSuccessListener{
+            response ->
+            autocompletelistString.clear()
+            for (prediction in response.autocompletePredictions) {
+             autocompletelistString.add(prediction.toString())
+            }
+        }.addOnFailureListener {autocompletelistString.clear()
+            autocompletelistString.add("fail")}
+    }
+    override fun onCameraMoveStarted(p0: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCameraMove() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCameraMoveCanceled() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCameraIdle() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        TODO("Not yet implemented")
     }
 }
