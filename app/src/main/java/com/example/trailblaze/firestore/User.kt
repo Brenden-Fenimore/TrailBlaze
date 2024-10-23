36ee150a9d42f5e602558de3ef6a184fc71973a3
lipeduh <fdesouza@student.fullsail.edu>
2024-10-22 20:52:51 -0400

    Implemented Mamie's logic for the created User class and UserManager

---
+++ app/src/main/java/com/example/trailblaze/firestore/User.kt
@@ -0,0 +1,16 @@
+package com.example.trailblaze.firestore
+
+data class User(
+    val uid: String = "",
+    val username: String = "",
+    val state: String = "",
+    val city: String = "",
+    val dateOfBirth: String = "",
+    val difficulty: String = "",
+    val distance: Double = 0.0,
+    val email: String = "",
+    val fitnessLevel: String = "",
+    val phone: String = "",
+    val zip: String = "",
+
+    )
