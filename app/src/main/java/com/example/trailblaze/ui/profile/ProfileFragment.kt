b78296fe47ea563f5e21bb4f791512f3c5fe0bbb
Mamie Gartin <megartin@student.fullsail.edu>
2024-10-23 14:00:15 -0400

    added an intent to navigate on the friendsAdapter to be able to click on the friend and go to their profile page

--- app/src/main/java/com/example/trailblaze/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/trailblaze/ui/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.trailblaze.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -81,6 +82,9 @@ class ProfileFragment : Fragment() {
         friendsList = mutableListOf()
 
         friendAdapter = FriendAdapter(friendsList) { friend ->
+            val intent = Intent(context, FriendsProfileActivity::class.java)
+            intent.putExtra("friendUserId", friend.userId)
+            startActivity(intent)
         }
         binding.friendsRecyclerView.adapter = friendAdapter
 
