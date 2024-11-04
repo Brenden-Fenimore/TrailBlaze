df4a3b6e971dca0e321917d9666d5b6a55f8c5f9
Mamie Gartin <megartin@student.fullsail.edu>
2024-11-04 00:11:35 -0500

    took off the report a bug... since the contact us and report a bug went to the same page

--- app/src/main/java/com/example/trailblaze/settings/SupportScreenActivity.kt
+++ app/src/main/java/com/example/trailblaze/settings/SupportScreenActivity.kt
@@ -25,7 +25,7 @@ class SupportScreenActivity : AppCompatActivity() {
 
         //initialize var
         contactUsTxt = findViewById(R.id.contactUs)
-        bugtxt = findViewById(R.id.bug)
+
 
         //set click listener for contactUs text
         contactUsTxt.setOnClickListener {
@@ -37,13 +37,6 @@ class SupportScreenActivity : AppCompatActivity() {
         //set the listener for the back button
         findViewById<ImageButton>(R.id.chevron_left).setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
-        }
-
-        //set the click listener for the bug text
-        bugtxt.setOnClickListener {
-            val intent = Intent(this, ContactUsActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
         }
 
         emergencyTxt = findViewById(R.id.emergency)
