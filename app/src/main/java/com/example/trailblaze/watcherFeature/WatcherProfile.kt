4dd5d3bd1fd8a927b93fee45847bba6db69ac0fd
FaeBae9 <156011872+FaeBae9@users.noreply.github.com>
2024-11-04 21:42:06 -0500

    -updated to get code to build

---
+++ app/src/main/java/com/example/trailblaze/watcherFeature/WatcherProfile.kt
@@ -0,0 +1,12 @@
+package com.example.trailblaze.watcherFeature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.trailblaze.R
+
+class WatcherProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_watcher_profile)
+    }
+}
