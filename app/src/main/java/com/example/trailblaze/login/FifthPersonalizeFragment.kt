b64c2a3a72c0b3ada550d9104a04fe9630169d0c
Mamie Gartin <megartin@student.fullsail.edu>
2024-11-07 16:54:30 -0500

    updated everything to include the watchers in the personalize fragments

--- app/src/main/java/com/example/trailblaze/login/FifthPersonalizeFragment.kt
+++ app/src/main/java/com/example/trailblaze/login/FifthPersonalizeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.trailblaze.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import com.example.trailblaze.MainActivity
 import com.example.trailblaze.R
 import com.example.trailblaze.databinding.FragmentEditWatcherProfileBinding
 import com.example.trailblaze.watcherFeature.EditWatcherProfile
@@ -19,12 +21,17 @@ class FifthPersonalizeFragment : Fragment() {
         //inflate the layout
         val view = inflater.inflate(R.layout.fragment_fifth_personalize, container, false)
         view.findViewById<Button>(R.id.yesBtn).setOnClickListener {
-            //Navigate to MainActivity
-            (activity as? PersonalizeActivity)?.loadFragment(FragmentEditWatcherProfileBinding)
+
         }
 
+        //set up the finished button
         view.findViewById<Button>(R.id.LaterBtn).setOnClickListener {
-            // Navigate to MainActivity
+            //navigate to MainActivity
+            val intent = Intent(activity, MainActivity::class.java)
+            //clear the activity stack so the user can't return to the fragments
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            activity?.finish()
         }
         return view
     }
