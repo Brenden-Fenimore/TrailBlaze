4b7aa965a4e6892fb34fad0b9207b6a484604efe
Mamie Gartin <megartin@student.fullsail.edu>
2024-11-10 10:45:21 -0500

    Had to remove some things from the manifest in order to create the apk... no worries everything is still there and works fine

--- app/src/main/java/com/example/trailblaze/ui/achievements/Achievement.kt
+++ app/src/main/java/com/example/trailblaze/ui/achievements/Achievement.kt
@@ -2,10 +2,10 @@ package com.example.trailblaze.ui.achievements
 
 // Data class representing an achievement in the application
 data class Achievement(
-    val id: String,
-    val name: String,
-    val description: String,
-    val criteria: Int,
-    val badge: Int,
+    val id: String = "",
+    val name: String = "",
+    val description: String = "",
+    val criteria: Int= 0,
+    val badge: Int = 0,
     var isUnlocked: Boolean = false
 )
--- app/src/main/java/com/example/trailblaze/ui/achievements/Badge.kt
+++ app/src/main/java/com/example/trailblaze/ui/achievements/Badge.kt
@@ -2,7 +2,7 @@ package com.example.trailblaze.ui.achievements
 
 // Data class representing a badge
 data class Badge(
-    val id: String,
-    val name: String,
-    val resourceId: Int,
+    val id: String = "",       // Default value of an empty string
+    val name: String = "",     // Default value of an empty string
+    val resourceId: Int = 0    // Default value of 0
 )
