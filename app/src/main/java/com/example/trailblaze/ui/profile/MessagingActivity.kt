df5891820f123345fa2ff59b209306f0eb871a10
FaeBae9 <156011872+FaeBae9@users.noreply.github.com>
2024-11-16 18:13:16 -0500

    -build with no errors -completed messaging files

--- app/src/main/java/com/example/trailblaze/ui/profile/MessagingActivity.kt
+++ app/src/main/java/com/example/trailblaze/ui/profile/MessagingActivity.kt
@@ -6,30 +6,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.trailblaze.R
 
 
 data class Message(val sendUserId: String, val text: String, val timestamp: Long)
 
 
-class MessagingActivity : Fragment() {
+class MessagingActivity : AppCompatActivity() {
 
-    companion object {
-        fun newInstance() = MessagingActivity()
-    }
-
-    private val viewModel: MessagingViewModel by viewModels()
+//    companion object {
+//        fun newInstance() = MessagingActivity()
+//    }
+    private lateinit var adapter: MessageAdapter
+    private lateinit var viewModel: MessagingViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.fragment_messaging)
         // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(MessagingViewModel::class.java)
+
+        adapter = MessageAdapter()
+        val messagesRecyclerView = findViewById<RecyclerView>(R.id.messagesRecyclerView)
+        messagesRecyclerView.layoutManager = LinearLayoutManager(this)
+        messagesRecyclerView.adapter = adapter
+
+        viewModel.messages.observe(this){ messages ->
+            adapter.submitList(messages)
+            messagesRecyclerView.scrollToPosition(messages.size - 1)
+        }
+
+        val messageEditText = findViewById<EditText>(R.id.messageEditText)
+        findViewById<Button>(R.id.sendMessageButton).setOnClickListener {
+            val messageContent = messageEditText.text.toString()
+            if(messageContent.isNotEmpty()){
+                viewModel.sendMessage(messageContent)
+                messageEditText.text.clear()
+            }
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_messaging, container, false)
-    }
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        return inflater.inflate(R.layout.fragment_messaging, container, false)
+//    }
 }
