5161e4199dba230e6783feeadef5a62af654d5dc
Mamie Gartin <megartin@student.fullsail.edu>
2025-03-03 18:20:54 -0500

    updated some things

--- app/src/main/java/com/example/trailblaze/settings/AboutActivity.kt
+++ app/src/main/java/com/example/trailblaze/settings/AboutActivity.kt
@@ -1,35 +1,21 @@
-package com.byteforce.trailblaze.settings
+package com.example.trailblaze.settings
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.byteforce.trailblaze.R
 
 
-class ContactUsActivity : AppCompatActivity() {
-
-    //declaration
-    private lateinit var submitBtn: Button
-
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_us)
+        setContentView(R.layout.activity_about)
         // Hide the ActionBar
         supportActionBar?.hide()
-
-        //find var by id
-        submitBtn=findViewById(R.id.submit)
 
         //set the listener for the back button
         findViewById<ImageButton>(R.id.chevron_left).setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
-
-        //set the click listener to close the screen
-        submitBtn.setOnClickListener {
-            finish()
-        }
-
     }
 }
